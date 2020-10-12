8679e32efbdf8e9c2318d0ff5c42a30faf9c40d8
Eric Demian Martinez Valdez <ericdemian.martinez@gmail.com>
2020-10-12 18:32:13 -0500

    Docs agregados

---
+++ docs/precache-manifest.ce8f0fec9c68c1f4b9b1ea28f96af5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511e106a5c8e5da955e5eeea54a6a70f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd50450eed82692cca42",
+    "url": "/static/css/main.c6c8c8ce.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd50450eed82692cca42",
+    "url": "/static/js/main.59b3889b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
